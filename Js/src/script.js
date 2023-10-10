117c06e06846b39a3157dc3a7534ceb3ea14ca58
g24-23543 <nonsiamonulla@gmail.com>
2023-10-10 20:48:58 -0300

    Prueba commit

---
+++ Js/src/script.js
@@ -0,0 +1,18 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
+
+// cosas a probar
+let suma=0;
+
+for(var i=0;i<=5;i++){
+
+  suma=suma+i;
+
+}
+
+console.log(i);
